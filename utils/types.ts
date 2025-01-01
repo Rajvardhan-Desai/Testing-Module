3da80ce364f1e7d174e95a000949effe4bdeb4d4
Rajvardhan Desai <103950321+Rajvardhan-Desai@users.noreply.github.com>
2025-01-01 07:45:06 +0000

    refactor types: update dropdown props and add new interfaces for question bank and match the pairs components

--- utils/types.ts
+++ utils/types.ts
@@ -1,16 +1,19 @@
-export type dropDownProps = {
-  id: string;
-  label: string;
-  options?: options[];
-  placeholder: string;
-  onChange?: () => Promise<void>;
-  value?: string;
-};
-
-export type options = {
-  label: string;
+interface Option {
   value: string;
-};
+  label: string;
+}
+
+export interface DropdownProps {
+  label: string;
+  options: Option[];
+  title?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  id: string;
+  containerClass?: string;
+  selectClass?: string;
+  disabled?: boolean;
+}
 
 export enum ROLE {
   Teacher = "teacher",
@@ -24,5 +27,48 @@ export interface ToggleGroupProps {
   onChange?: (value: ROLE) => void;
 }
 
+// Create Test Page
 
+export interface MatchThePairs_FieldProps {
+  title: string;
+  isRightSide?: boolean;
+  values: Record<string, string>;
+  onValueChange: (key: string, value: string) => void;
+  onConnect: (key: string) => void;
+  activeItem?: string | null;
+  connections?: Record<string, string>;
+}
 
+export interface MatchThePairs_FieldItemProps {
+  label: string;
+  isRight?: boolean;
+  value: string;
+  onChange: (value: string) => void;
+  id: string;
+  isActive?: boolean;
+  onSelect: () => void;
+}
+
+// ------------------------------
+
+// Question Bank Page
+
+export interface QuestionProps {
+  questionNumber: string;
+  description: string;
+}
+
+export interface QuestionListProps {
+  questions: QuestionProps[];
+  selectedQuestionIndex: number;
+  onQuestionSelect: (index: number) => void;
+}
+
+export interface ScrollbarProps {
+  contentHeight: number; // Height of the scrollbar track
+  scrollPosition: number; // A value between 0 and 1
+  onScroll: (position: number) => void;
+  totalContent: number; // Total number of items or total scrollable units
+  visibleContent: number; // Number of visible items or units
+}
+// ------------------------------
