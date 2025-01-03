113e3ccee0797c4df48522eb5a63066bd1c4bd7b
Rajvardhan Desai <103950321+Rajvardhan-Desai@users.noreply.github.com>
2025-01-03 13:59:25 +0000

    refactor: update headers with updated dropdowns for class, subject, lesson, and homework

--- components/QuestionBankHeader.tsx
+++ components/QuestionBankHeader.tsx
@@ -1,32 +1,99 @@
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
+import Dropdown from "@/components/Dropdown/Dropdown";
+import { useCallback, useMemo, useState } from "react";
 
 export default function QuestionBankHeader() {
+  const [selection, setSelection] = useState({
+    class: "५",
+    subject: "विषय १",
+    lesson: "धडा १",
+    homework: "स्वाध्याय १",
+  });
+
+  const handleSelect = useCallback(
+    (value: string | number, dropdownId: string) => {
+      console.log(`Dropdown ID: ${dropdownId}, Selected value: ${value}`);
+
+      // Update the selection state based on the dropdownId
+      setSelection((prevSelection) => ({
+        ...prevSelection,
+        [dropdownId]: value,
+      }));
+    },
+    [setSelection]
+  );
+
+  const classOptions = useMemo(() => ["५", "६", "७", "८", "९", "१०"], []);
+
+  const subjectOptions = useMemo(() => ["विषय १", "विषय २", "विषय ३"], []);
+
+  const lessonOptions = useMemo(() => ["धडा १", "धडा २", "धडा ३"], []);
+
+  const homeworkOptions = useMemo(() => ["स्वाध्याय १", "स्वाध्याय २"], []);
+
   return (
     <div className="bg-[#6378fd] text-white flex flex-col items-center p-4 rounded-lg shadow">
-      <div className="flex items-center justify-center w-full text-center gap-8" >
+      <div className="flex items-center justify-center w-full text-center gap-8">
         <Image src="/question.svg" alt="test-paper" width={100} height={100} />
         <h1 className="text-3xl rozha-one-bold">प्रश्न संच</h1>
         <Image src="/paper.svg" alt="test-paper" width={80} height={80} />
       </div>
 
-      <div className="flex items-center justify-between flex-wrap w-full mt-4 gap-2 no-wrap laila-regular">
-        {["इयत्ता", "विषय", "धडा", "स्वाध्याय"].map((label, index) => (
-          <div
-            key={index}
-            className="bg-[#fc708a] rounded-2xl py-1 px-4 items-center gap-2 border-2 border-white flex flex-col md:flex-row md:w-[20%] w-full "
-          >
-            <label className="text-xl font-light">{label}:</label>
-            <select
-              title={label}
-              className="px-4 py-1 shadow-md rounded-xl text-black appearance-none mt-2 md:mt-0 w-[80%] text-center"
-            >
-              <option value="१">१</option>
-              <option value="२">२</option>
-            </select>
-          </div>
-        ))}
+      <div className="flex flex-col sm:flex-row justify-between w-full mt-4 gap-2 laila-regular">
+        <Dropdown
+          id="class-dropdown"
+          items={classOptions}
+          label="प्रकार:"
+          defaultValue={selection.class}
+          buttonBgColor="bg-[#fc708a]"
+          buttonBorderColor="border-white"
+          buttonBorderWidth="border-[2px]"
+          onSelect={(value) => handleSelect(value, "class-dropdown")}
+          className="w-full sm:w-[20%]"
+        />
+
+        <Dropdown
+          id="subject-dropdown"
+          label="विषय:"
+          items={subjectOptions}
+          defaultValue={selection.subject}
+          buttonBgColor="bg-[#fc708a]"
+          buttonBorderColor="border-white"
+          buttonBorderWidth="border-[2px]"
+          onSelect={(value) => handleSelect(value, "subject-dropdown")}
+          className="w-full sm:w-[20%]"
+        />
+
+        <Dropdown
+          id="lesson-dropdown"
+          label="धडा:"
+          items={lessonOptions}
+          defaultValue={selection.lesson}
+          buttonBgColor="bg-[#fc708a]"
+          buttonBorderColor="border-white"
+          buttonBorderWidth="border-[2px]"
+          onSelect={(value) => handleSelect(value, "lesson-dropdown")}
+          className="w-full sm:w-[20%]"
+          allowAddOption
+          allowAddOptionText={"add lesson"}
+        />
+
+        <Dropdown
+          id="homework-dropdown"
+          label="स्वाध्याय:"
+          items={homeworkOptions}
+          defaultValue={selection.homework}
+          buttonBgColor="bg-[#fc708a]"
+          buttonBorderColor="border-white"
+          buttonBorderWidth="border-[2px]"
+          onSelect={(value) => handleSelect(value, "homework-dropdown")}
+          className="w-full sm:w-[20%]"
+          allowAddOption
+          allowAddOptionText={"add homework"}
+        />
+        
       </div>
-    
     </div>
   );
 }
--- components/TestHeader.tsx
+++ components/TestHeader.tsx
@@ -1,108 +1,136 @@
 "use client";
-import React, { useState } from "react";
-import Dropdown from "@/components/Dropdown";
+import React, { useCallback, useMemo, useState } from "react";
+import Dropdown from "@/components/Dropdown/Dropdown";
 import Image from "next/image";
 
 export default function TestHeader() {
+  // Initialize selection state
   const [selection, setSelection] = useState({
-    class: "१",
+    class: "५",
     subject: "विषय १",
     lesson: "धडा १",
     homework: "स्वाध्याय १",
   });
 
-  const handleChange =
-    (field: keyof typeof selection) =>
-    (event: React.ChangeEvent<HTMLSelectElement>) => {
-      setSelection({ ...selection, [field]: event.target.value });
-    };
+  /**
+   * Handler for dropdown selection changes.
+   * @param value - The selected value from the dropdown.
+   * @param dropdownKey - The key corresponding to the dropdown (e.g., 'class', 'subject').
+   */
+  const handleSelect = useCallback(
+    (value: string | number, dropdownKey: string) => {
+      console.log(`Dropdown Key: ${dropdownKey}, Selected value: ${value}`);
 
-  const classOptions = [
-    { value: "१", label: "१" },
-    { value: "२", label: "२" },
-    { value: "३", label: "३" },
-  ];
+      // Update the selection state based on the dropdownKey
+      setSelection((prevSelection) => ({
+        ...prevSelection,
+        [dropdownKey]: value,
+      }));
+    },
+    []
+  );
 
-  const subjectOptions = [
-    { value: "विषय १", label: "विषय १" },
-    { value: "विषय २", label: "विषय २" },
-    { value: "विषय ३", label: "विषय ३" },
-  ];
-
-  const lessonOptions = [
-    { value: "धडा १", label: "धडा १" },
-    { value: "धडा २", label: "धडा २" },
-    { value: "धडा ३", label: "धडा ३" },
-  ];
-
-  const homeworkOptions = [
-    { value: "स्वाध्याय १", label: "स्वाध्याय १" },
-    { value: "स्वाध्याय २", label: "स्वाध्याय २" },
-  ];
+  // Define dropdown options using useMemo for performance optimization
+  const classOptions = useMemo(() => ["५", "६", "७", "८", "९", "१०"], []);
+  const subjectOptions = useMemo(() => ["विषय १", "विषय २", "विषय ३"], []);
+  const lessonOptions = useMemo(() => ["धडा १", "धडा २", "धडा ३"], []);
+  const homeworkOptions = useMemo(() => ["स्वाध्याय १", "स्वाध्याय २"], []);
 
   return (
     <div className="text-white rounded-lg">
       <div className="flex flex-col md:flex-row gap-2">
+        {/* Left Section: Dropdowns and Header */}
         <div className="flex flex-col items-center p-4 rounded-lg shadow bg-[#6378fd] w-full md:w-1/2">
           {/* Header Section */}
           <div className="flex items-center justify-center w-full text-center gap-8">
-            <Image src="/test-paper.png" alt="test-paper" width={70} height={70}/>
+            <Image
+              src="/test-paper.png"
+              alt="test-paper"
+              width={70}
+              height={70}
+            />
             <h1 className="text-3xl rozha-one-bold">चाचणी तयार करा</h1>
           </div>
 
-          {/* Left Section (Input Fields) */}
-          <div className="flex flex-wrap justify-between w-full mt-4 gap-2">
+          {/* Dropdowns Section */}
+          <div className="flex flex-wrap justify-between w-full mr-3 ml-3 gap-2">
+            {/* Class Dropdown */}
             <Dropdown
               id="class-dropdown"
+              items={classOptions}
               label="इयत्ता:"
-              title="Select Class"
-              options={classOptions}
-              value={selection.class}
-              onChange={handleChange("class")}
+              selected={selection.class}
+              buttonBgColor="bg-[#fc708a]"
+              buttonBorderColor="border-white"
+              buttonBorderWidth="border-[2px]"
+              onSelect={(value) => handleSelect(value, "class")}
+              className="sm:w-[48%]"
+              
             />
 
+            {/* Subject Dropdown */}
             <Dropdown
               id="subject-dropdown"
               label="विषय:"
-              title="Select Subject"
-              options={subjectOptions}
-              value={selection.subject}
-              onChange={handleChange("subject")}
+              items={subjectOptions}
+              selected={selection.subject}
+              buttonBgColor="bg-[#fc708a]"
+              buttonBorderColor="border-white"
+              buttonBorderWidth="border-[2px]"
+              onSelect={(value) => handleSelect(value, "subject")}
+              className="sm:w-[48%]"
             />
 
+            {/* Lesson Dropdown */}
             <Dropdown
               id="lesson-dropdown"
               label="धडा:"
-              title="Select Lesson"
-              options={lessonOptions}
-              value={selection.lesson}
-              onChange={handleChange("lesson")}
+              items={lessonOptions}
+              selected={selection.lesson}
+              buttonBgColor="bg-[#fc708a]"
+              buttonBorderColor="border-white"
+              buttonBorderWidth="border-[2px]"
+              onSelect={(value) => handleSelect(value, "lesson")}
+              className="sm:w-[48%]"
+              allowAddOption
+              allowAddOptionText="add lesson"
             />
 
+            {/* Homework Dropdown */}
             <Dropdown
               id="homework-dropdown"
               label="स्वाध्याय:"
-              title="Select Homework"
-              options={homeworkOptions}
-              value={selection.homework}
-              onChange={handleChange("homework")}
+              items={homeworkOptions}
+              selected={selection.homework}
+              buttonBgColor="bg-[#fc708a]"
+              buttonBorderColor="border-white"
+              buttonBorderWidth="border-[2px]"
+              onSelect={(value) => handleSelect(value, "homework")}
+              className="sm:w-[48%]"
+              allowAddOption
+              allowAddOptionText="add homework"
             />
           </div>
         </div>
 
+        {/* Right Section: Visual Representation */}
         <div className="flex flex-col p-4 rounded-lg shadow bg-[#6378fd] w-full md:w-1/2">
           <div className="grid grid-cols-7 gap-4 p-4">
-            {/* First row */}
+            {/* First Item */}
             <div className="flex items-center">
               <div className="flex items-center justify-center bg-green-400 w-10 h-10 text-white rounded-full font-bold">
                 1
               </div>
             </div>
+
+            {/* Plus Icon */}
             <div className="flex items-center">
               <div className="flex items-center justify-center bg-[#a6b1ff] w-10 h-10 text-white rounded-full font-bold">
                 +
               </div>
             </div>
+
+            {/* Additional Items */}
             {[...Array(19)].map((_, index: number) => (
               <div
                 key={index + 2}
