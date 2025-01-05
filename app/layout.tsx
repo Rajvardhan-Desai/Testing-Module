fe67c8bff3549650c3b747d257188b068e2274e8
Rajvardhan Desai <103950321+Rajvardhan-Desai@users.noreply.github.com>
2025-01-05 13:54:41 +0000

    refactor: update font imports and styles in globals.css and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,31 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rozha_One,Laila } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/utils/AuthProvider";
 import { ToastProvider } from "@/components/ui/ToastProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+const rozhaOne = Rozha_One({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-rozha-one',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const laila = Laila({
+  weight: ['300','400','500','600','700'],
+  subsets: ['latin'],
+  variable: '--font-laila',
 });
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Parikhsa Mitra",
@@ -27,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${rozhaOne.variable} ${laila.variable}`}
         cz-shortcut-listen="true"
       >
         <ToastProvider>
